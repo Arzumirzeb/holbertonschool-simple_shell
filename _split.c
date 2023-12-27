f0b8b5e165754a2839a4428120059b5c71bb8765
heydarov93 <heyderov.yashar@mail.ru>
2023-12-27 02:55:27 -0800

    function for splitting command into array added

---
+++ _split.c
@@ -0,0 +1,51 @@
+#include "header.h"
+
+/**
+ * _split - splits given string into array
+ * @str: string to split
+ * @delim: separator, delimiter
+ *
+ * Return: pointer to two dimensional array
+ */
+
+char **_split(char *str, char *delim)
+{
+	char *cp_str, *word;
+	char **arr;
+	int i;
+
+	cp_str = strdup(str);
+
+	word = strtok(cp_str, delim);
+
+	if (word != NULL)
+	{
+		arr = malloc(sizeof(char *) * 1);
+
+		if (arr == NULL)
+		{
+			free(arr);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; 1; i++)
+	{
+		arr[i] = word;
+
+		word = strtok(NULL, delim);
+		if (word != NULL)
+		{
+			arr = (char **) realloc(arr, sizeof(char *) * 1);
+			if (arr == NULL)
+			{
+				free(arr);
+				return (NULL);
+			}
+		}
+		else
+			break;
+	}
+
+	return (arr);
+}
