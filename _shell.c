7a3b4ca0f77e0d8506670066ae51d7207ddce307
heydarov93 <heyderov.yashar@mail.ru>
2023-12-30 02:22:41 -0800

    create_full_path function created to find full path according single command

--- _shell.c
+++ _shell.c
@@ -2,6 +2,50 @@
 #include <sys/wait.h>
 
 /**
+ * create_full_path - creates full path from single command
+ * @command: single command
+ */
+
+void create_full_path(char **command)
+{
+	int length_command = strlen(*command);
+	char *tmp, *single_path, *path;
+	size_t size_tmp;
+
+	path = strdup(getenv("PATH"));
+	single_path = strtok(path, ":");
+
+	while (single_path != NULL)
+	{
+		size_tmp = strlen(single_path) + length_command + 2;
+		tmp = malloc(sizeof(char) * size_tmp);
+
+		if (tmp == NULL)
+		{
+			free(tmp);
+			return;
+		}
+
+		strcpy(tmp, single_path);
+		strcat(tmp, "/");
+		strcat(tmp, *command);
+
+		if (access(tmp, X_OK) == -1)
+		{
+			free(tmp);
+			single_path = strtok(NULL, ":");
+		}
+		else
+		{
+			free(*command);
+			*command = tmp;
+			return;
+		}
+	}
+}
+
+
+/**
  * main - entry point
  * @ac: count of arguments
  * @av: array of arguments values
@@ -11,8 +55,7 @@
 
 int main(int ac, char **av)
 {
-	char **argv;
-	char *line;
+	char **argv, *line;
 	int status, i;
 	pid_t fork_id;
 	(void)ac;
@@ -22,29 +65,29 @@ int main(int ac, char **av)
 		line = _getline();
 		if (!line)
 			break;
-
 		argv = _split(line, " \n\t");
 		free(line);
-
 		if (!*argv)
 		{
 			free(argv);
 			continue;
 		}
+
+		create_full_path(&argv[0]);
 		fork_id = fork();
 		if (fork_id == 0)
 		{
 			if (execve(argv[0], argv, environ) == -1)
 			{
+				for (i = 0; argv[i]; i++)
+					free(argv[i]);
 				free(argv);
 				fprintf(stderr, "%s: %s\n", av[0], strerror(errno));
 			}
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 		else
-		{
 			wait(&status);
-		}
 		for (i = 0; argv[i]; i++)
 			free(argv[i]);
 		free(argv);
